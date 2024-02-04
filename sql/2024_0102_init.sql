d13e60be19df548e89694d8804e4fa48bc80cb8b
Mitsuru Kusumoto <kusumoto.mitsuru@gmail.com>
2024-02-04 21:40:41 +0900

    admin tool

--- sql/2024_0102_init.sql
+++ sql/2024_0102_init.sql
@@ -7,17 +7,20 @@ CREATE INDEX ON aoj_acceptances (aoj_userid);
 CREATE INDEX ON aoj_acceptances (problem_id);
 
 CREATE TABLE problems (
-  id        INTEGER NOT NULL,
-  name      VARCHAR(255) NOT NULL,
-  lang      INTEGER NOT NULL,
-  plevel    INTEGER NOT NULL,
-  org       VARCHAR(64) NOT NULL,
-  used_in   VARCHAR(64) NOT NULL,
-  slot      VARCHAR(64) NOT NULL,
-  meta      TEXT NOT NULL,
-  PRIMARY KEY (id)
+  problem_id   INTEGER NOT NULL,
+  name         VARCHAR(255) NOT NULL,
+  contest_type INTEGER NOT NULL,
+  level       INTEGER NOT NULL,
+  org          VARCHAR(64) NOT NULL,
+  used_in      VARCHAR(64) NOT NULL,
+  slot         VARCHAR(64) NOT NULL,
+  ja           INTEGER NOT NULL,
+  en           INTEGER NOT NULL,
+  inherited_likes INTEGER NOT NULL,
+  meta         TEXT NOT NULL,
+  PRIMARY KEY (problem_id)
 );
-CREATE INDEX ON problems (id);
+CREATE INDEX ON problems (problem_id);
 
 CREATE TABLE user_points (
   aoj_userid        VARCHAR(64) NOT NULL,
@@ -35,3 +38,9 @@ CREATE TABLE likes (
   PRIMARY KEY (github_id)
 );
 CREATE INDEX ON likes (problem_id);
+
+CREATE TABLE like_counts (
+  problem_id  INTEGER NOT NULL,
+  weight      INTEGER NOT NULL DEFAULT 0,
+  PRIMARY KEY (problem_id)
+);
